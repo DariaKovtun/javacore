5fe5ca5ed5ebdfbd468d06b2f13acc371576d802
Ruslan Korman <testourpact@gmail.com>
2016-11-13 19:49:15 +0200

    lesson 2

--- src/main/java/com/dariakovtun/app/App.java
+++ src/main/java/com/dariakovtun/app/App.java
@@ -1,13 +1,15 @@
 package com.dariakovtun.app;
-
+import java.util.Scanner;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+   // public static void main( String[] args )
+  // {
+    //   System.out.println("Hello World!");
+       //   }
+//   }
+
     }
-}
---
+++ src/main/java/com/dariakovtun/app/classwork3/lesson3/WideningCasting.java
@@ -0,0 +1,27 @@
+package com.dariakovtun.app.classwork3.lesson3;
+
+
+public class WideningCasting {
+ static   byte byte1 = 127;
+
+    static   short short1 = 0;
+
+    static int int1 = 0;
+
+    static long long1 = 0L;
+
+    static float float1 = 0.0f;
+
+    static double double1 = 0.0d;
+
+//correct conversion 64f<=32f<=64<=32<=16<=8
+
+    public static void Main (){
+
+
+        double1 = float1 = long1 = int1 = short1 = byte1;
+
+        System.out.println(double1);
+
+    }
+}
